3ceae1e951e4538aead5a7cf59a6375f8dcc196c
Ian Vu <ianv6207@mytusd.org>
2026-04-02 07:48:26 -0700

    Create content.js

---
+++ content.js
@@ -0,0 +1,10 @@
+document.body.innerHTML = '';
+document.head.innerHTML = "<title>Simple Text Editor - Offline</title>";
+document.body.style = `
+background-color: black;
+color: white;
+font-family: Courier New;
+font-weight: bold;
+font-size: 17pt;
+`;
+document.body.contentEditable = "true";
